141f7dbb5ffdde2f9a243686a36dc86912d4ac3b
Bill <jfg@163.com>
2023-07-31 23:44:52 +0800

    Remember used markers

--- AutoMark.lua
+++ AutoMark.lua
@@ -4,34 +4,47 @@ f:RegisterEvent("PARTY_LOOT_METHOD_CHANGED")
 f:SetScript("OnEvent", function()
   if IsPartyLeader() then
     -- Get party members
-    local partyMembers = {"player"}
-    for n = 1, GetNumPartyMembers() do
-      table.insert(partyMembers, "party"..n)
+    local partyMembers = {{"player", 0}}
+    if UnitExists("pet") then
+      table.insert(partyMembers, {"pet", 0})
     end
-    -- Get available markers
-    local raidTarget = {1, 1, 1, 1, 1, 1, 1, 1}
+    for n = 1, GetNumPartyMembers() do
+      table.insert(partyMembers, {"party"..n, 0})
+      if UnitExists("partypet"..n) then
+        table.insert(partyMembers, {"partypet"..n, 0})
+      end
+    end
+    -- Get available markers and used markers
+    local raidTarget = {0, 0, 0, 0, 0, 0, 0, 0}
+    for k, v in pairs(partyMembers) do
+      local i = GetRaidTargetIndex(v[1])
+      if i then
+        v[2] = i
+      end
+    end
     -- Mark function
     function Mark(k, v, i)
-      if raidTarget[i] == 1 then
-        if i == 4 then
-          v = string.gsub(string.gsub(v, "player", "pet"), "party", "partypet")
+      if raidTarget[i] == 0 then
+        if (v[2] == 0) then
+          v[2] = i
         else
-          partyMembers[k] = nil
+          i = v[2]
         end
-        SetRaidTarget(v, i)
-        raidTarget[i] = 0
+        SetRaidTarget(v[1], i)
+        raidTarget[i] = 1
       end
     end
     -- Priority marking by class
     for k, v in pairs(partyMembers) do
-      local _, c = UnitClass(v)
-      if c == "MAGE" or c == "ROGUE" then
+      local _, c = UnitClass(v[1])
+      if string.find(v[1], "pet") then
+        Mark(k, v, 4)
+      elseif c == "MAGE" or c == "ROGUE" then
         Mark(k, v, 1)
       elseif c == "WARRIOR" then
         Mark(k, v, 2)
       elseif c == "HUNTER" or c == "WARLOCK" then
         Mark(k, v, 3)
-        Mark(k, v, 4)
       elseif c == "PRIEST" or c == "DRUID" then
         Mark(k, v, 5)
       else
@@ -39,14 +52,14 @@ f:SetScript("OnEvent", function()
       end
     end
     -- Triangle for pet only
-    raidTarget[4] = 0
+    raidTarget[4] = 1
     -- Using the remaining markers
     for k, v in pairs(partyMembers) do
-      if v then
+      if v[2] == 0 then
         for i in pairs(raidTarget) do
-          if raidTarget[i] == 1 then
-            SetRaidTarget(v, i)
-            raidTarget[i] = 0
+          if raidTarget[i] == 0 then
+            SetRaidTarget(v[1], i)
+            raidTarget[i] = 1
             break
           end
         end
