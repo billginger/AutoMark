e890dc141ea244d41f909ec88bb9ab50f0d1724b
Bill <jfg@163.com>
2023-07-31 21:30:51 +0800

    Mark all party members

--- AutoMark.lua
+++ AutoMark.lua
@@ -3,23 +3,53 @@ local f = CreateFrame("Frame")
 f:RegisterEvent("PARTY_LOOT_METHOD_CHANGED")
 f:SetScript("OnEvent", function()
   if IsPartyLeader() then
-    local t = {"player", "pet"}
+    -- Get party members
+    local partyMembers = {"player"}
     for n = 1, GetNumPartyMembers() do
-      table.insert(t, "party"..n)
-      table.insert(t, "partypet"..n)
+      table.insert(partyMembers, "party"..n)
     end
-    for k, v in pairs(t) do
+    -- Get available markers
+    local raidTarget = {1, 1, 1, 1, 1, 1, 1, 1}
+    -- Mark function
+    function Mark(k, v, i)
+      if raidTarget[i] == 1 then
+        if i == 4 then
+          v = string.gsub(string.gsub(v, "player", "pet"), "party", "partypet")
+        else
+          partyMembers[k] = nil
+        end
+        SetRaidTarget(v, i)
+        raidTarget[i] = 0
+      end
+    end
+    -- Priority marking by class
+    for k, v in pairs(partyMembers) do
       local _, c = UnitClass(v)
-      if c == "WARRIOR" then
-        SetRaidTarget(v, 2)
+      if c == "MAGE" or c == "ROGUE" then
+        Mark(k, v, 1)
+      elseif c == "WARRIOR" then
+        Mark(k, v, 2)
       elseif c == "HUNTER" or c == "WARLOCK" then
-        SetRaidTarget(v, 3)
+        Mark(k, v, 3)
+        Mark(k, v, 4)
       elseif c == "PRIEST" or c == "DRUID" then
-        SetRaidTarget(v, 5)
-      elseif c == "MAGE" or c == "ROGUE" then
-        SetRaidTarget(v, 1)
+        Mark(k, v, 5)
       else
-        SetRaidTarget(v, 6)
+        Mark(k, v, 6)
+      end
+    end
+    -- Triangle for pet only
+    raidTarget[4] = 0
+    -- Using the remaining markers
+    for k, v in pairs(partyMembers) do
+      if v then
+        for i in pairs(raidTarget) do
+          if raidTarget[i] == 1 then
+            SetRaidTarget(v, i)
+            raidTarget[i] = 0
+            break
+          end
+        end
       end
     end
   end
